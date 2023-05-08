8b9ddadbef4ec99e16cccb493a46eaa6f8a33179
David Whipple <dwhipp88@gmail.com>
2023-05-08 17:09:58 -0600

    setup basic landing page layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 const App = () => {
-    return <div>Hello World</div>;
+
+    const directories = [
+        {
+            id: 1,
+            title: 'Hats',
+            
+        }
+    ]
+
+    return (
+        <div className="landing-container">
+            <div className="address-bar-container">
+                <div className="website-title-container">
+                    <h1>Blue Letter Boutique</h1>
+                </div>
+                <div className="navigation-container">
+                    <ul>
+                        <li>WHAT'S NEW</li>
+                        <li>DRESSES</li>
+                        <li>SWIM</li>
+                        <li>CLOTHING</li>
+                        <li>JEWELRY</li>
+                        <li>ACCESSORIES</li>
+                        <li>SHOES</li>
+                        <li>SALE</li>
+                    </ul>
+                </div>
+                <div className="search-box-container">
+                    <p>I'm looking for...</p>
+                </div>
+                <div className="my-account-container">MY ACCOUNT</div>
+                <div className="shop-icon-container">FAVICON</div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
