6537f02996ad86783257c7d07eeabed5a8d7f6d5
David Whipple <dwhipp88@gmail.com>
2023-05-08 19:49:18 -0600

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-const App = () => {
+import './App.styles.css';
 
-    const directories = [
-        {
-            id: 1,
-            title: 'Hats',
-            
-        }
-    ]
+const App = () => {
+    // const directories = [
+    //     {
+    //         id: 1,
+    //         title: 'Hats',
+
+    //     }
+    // ]
 
     return (
-        <div className="landing-container">
-            <div className="address-bar-container">
-                <div className="website-title-container">
-                    <h1>Blue Letter Boutique</h1>
-                </div>
-                <div className="navigation-container">
-                    <ul>
-                        <li>WHAT'S NEW</li>
-                        <li>DRESSES</li>
-                        <li>SWIM</li>
-                        <li>CLOTHING</li>
-                        <li>JEWELRY</li>
-                        <li>ACCESSORIES</li>
-                        <li>SHOES</li>
-                        <li>SALE</li>
-                    </ul>
-                </div>
-                <div className="search-box-container">
-                    <p>I'm looking for...</p>
-                </div>
-                <div className="my-account-container">MY ACCOUNT</div>
-                <div className="shop-icon-container">FAVICON</div>
+        <div className="address-bar-container">
+            <div className="website-title-container">
+                <h1>Blue Letter Boutique</h1>
             </div>
+            <div className="navigation-container">
+                <ul>
+                    <li>WHAT'S NEW</li>
+                    <li>DRESSES</li>
+                    <li>SWIM</li>
+                    <li>CLOTHING</li>
+                    <li>JEWELRY</li>
+                    <li>ACCESSORIES</li>
+                    <li>SHOES</li>
+                    <li>SALE</li>
+                </ul>
+            </div>
+            <div className="search-box-container">
+                <input
+                    className="search-box"
+                    type="search"
+                    placeholder="I'm looking for..."
+                />
+            </div>
+            <div className="my-account-container">MY ACCOUNT</div>
+            <div className="shop-icon-container">FAVICON</div>
         </div>
     );
 };
