7f7d0a26a63ad03890e57b94f1aa359708340d4d
David Whipple <dwhipp88@gmail.com>
2023-05-09 19:59:14 -0600

    working on jumbotron

--- src/App.js
+++ src/App.js
@@ -11,36 +11,44 @@ const App = () => {
 
     return (
         <>
-        <div className='sales-bar-container'>
-            <h2>SPEND $100 FOR FREE SHIPPING</h2>
-            <p>Ground shipping, continental US only</p>
-        </div>
-        <div className="address-bar-container">
-            <div className="website-title-container">
-                <h1>Blue Letter Boutique</h1>
+            <div className="sales-bar-container">
+                <h2>SPEND $100 FOR FREE SHIPPING</h2>
+                <p>Ground shipping, continental US only</p>
             </div>
-            <div className="navigation-container">
-                <ul>
-                    <li>WHAT'S NEW</li>
-                    <li>DRESSES</li>
-                    <li>SWIM</li>
-                    <li>CLOTHING</li>
-                    <li>JEWELRY</li>
-                    <li>ACCESSORIES</li>
-                    <li>SHOES</li>
-                    <li>SALE</li>
-                </ul>
+            <div className="address-bar-container">
+                <div className="website-title-container">
+                    <h1>Blue Letter Boutique</h1>
+                </div>
+                <div className="navigation-container">
+                    <ul>
+                        <li>WHAT'S NEW</li>
+                        <li>DRESSES</li>
+                        <li>SWIM</li>
+                        <li>CLOTHING</li>
+                        <li>JEWELRY</li>
+                        <li>ACCESSORIES</li>
+                        <li>SHOES</li>
+                        <li>SALE</li>
+                    </ul>
+                </div>
+                <div className="search-box-container">
+                    <input
+                        className="search-box"
+                        type="search"
+                        placeholder="I'm looking for..."
+                    />
+                </div>
+                <div className="my-account-container">MY ACCOUNT</div>
+                <div className="shop-icon-container">FAVICON</div>
             </div>
-            <div className="search-box-container">
-                <input
-                    className="search-box"
-                    type="search"
-                    placeholder="I'm looking for..."
-                />
+            <div className="jumbotron-container">
+                <div className="jumbotron-text">
+                    <h2>
+                        BEAUTY <br /> IN STYLE
+                    </h2>
+                    <p>SHOP THE COLLECTION</p>
+                </div>
             </div>
-            <div className="my-account-container">MY ACCOUNT</div>
-            <div className="shop-icon-container">FAVICON</div>
-        </div>
         </>
     );
 };
