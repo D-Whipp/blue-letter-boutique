773f94e575eb1293ceeb7f9775feaeddda6781ec
David Whipple <dwhipp88@gmail.com>
2023-05-18 20:22:15 -0600

    created and styled our story section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import OurStory from './components/our-story/our-story.component';
 // import './App.styles.css';
+
 
 const App = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
+            <Route path='/our-story' element={<OurStory />} />
         </Routes>
     );
 };
---
+++ src/components/our-story/our-story.component.jsx
@@ -0,0 +1,106 @@
+// import {Routes, Route} from 'react-router-dom';
+
+import './our-story.styles.css';
+
+import Promotion from '../../routes/promotion/promotion.component';
+import Navigation from '../../routes/navigation/navigation.component';
+import QuickLinks from '../../routes/quick-links/quick-links.component';
+import Footer from '../footer/footer.component';
+
+const OurStory = () => {
+    return (
+        <>
+            <Promotion />
+            <Navigation />
+            <div className="our-story-container">
+                <h2>our story</h2>
+                <h3>"We sell beauty"</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Consequuntur perspiciatis nihil totam, earum
+                    similique iste quisquam, quaerat nostrum omnis,
+                    quo debitis non necessitatibus quae! Nobis aliquam
+                    accusantium repellendus inventore similique
+                    possimus, incidunt cum modi.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Rem nisi aperiam totam ipsum, eaque
+                    similique dolore. Illo numquam atque autem at
+                    asperiores dolores, maxime ratione, voluptates
+                    illum dignissimos perspiciatis ducimus commodi
+                    natus corporis animi quidem rem harum consequatur
+                    vel? Distinctio commodi saepe dolores a?
+                    Perspiciatis soluta harum optio illo repellendus
+                    fugit. Itaque autem neque vel tempora facilis
+                    incidunt inventore, quisquam optio illo dicta.
+                    Veritatis impedit incidunt sapiente ex alias saepe
+                    aut voluptates officia animi obcaecati, architecto
+                    aliquam dolores voluptate sequi dicta libero
+                    repellendus fugiat. Earum consequatur enim nobis
+                    neque distinctio eum commodi ipsa voluptatem.
+                    Voluptatibus cum eveniet aliquam laudantium quasi?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Consequuntur perspiciatis nihil totam, earum
+                    similique iste quisquam, quaerat nostrum omnis,
+                    quo debitis non necessitatibus quae! Nobis aliquam
+                    accusantium repellendus inventore similique
+                    possimus, incidunt cum modi.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Rem nisi aperiam totam ipsum, eaque
+                    similique dolore. Illo numquam atque autem at
+                    asperiores dolores, maxime ratione, voluptates
+                    illum dignissimos perspiciatis ducimus commodi
+                    natus corporis animi quidem rem harum consequatur
+                    vel? Distinctio commodi saepe dolores a?
+                    Perspiciatis soluta harum optio illo repellendus
+                    fugit. Itaque autem neque vel tempora facilis
+                    incidunt inventore, quisquam optio illo dicta.
+                    Veritatis impedit incidunt sapiente ex alias saepe
+                    aut voluptates officia animi obcaecati, architecto
+                    aliquam dolores voluptate sequi dicta libero
+                    repellendus fugiat. Earum consequatur enim nobis
+                    neque distinctio eum commodi ipsa voluptatem.
+                    Voluptatibus cum eveniet aliquam laudantium quasi?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Rem nisi aperiam totam ipsum, eaque
+                    similique dolore. Illo numquam atque autem at
+                    asperiores dolores, maxime ratione, voluptates
+                    illum dignissimos perspiciatis ducimus commodi
+                    natus corporis animi quidem rem harum consequatur
+                    vel? Distinctio commodi saepe dolores a?
+                    Perspiciatis soluta harum optio illo repellendus
+                    fugit. Itaque autem neque vel tempora facilis
+                    incidunt inventore, quisquam optio illo dicta.
+                    Veritatis impedit incidunt sapiente ex alias saepe
+                    aut voluptates officia animi obcaecati, architecto
+                    aliquam dolores voluptate sequi dicta libero
+                    repellendus fugiat. Earum consequatur enim nobis
+                    neque distinctio eum commodi ipsa voluptatem.
+                    Voluptatibus cum eveniet aliquam laudantium quasi?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Consequuntur perspiciatis nihil totam, earum
+                    similique iste quisquam, quaerat nostrum omnis,
+                    quo debitis non necessitatibus quae! Nobis aliquam
+                    accusantium repellendus inventore similique
+                    possimus, incidunt cum modi.
+                </p>
+                <p className="our-story-signature">XOXO,</p>
+                <p className="our-story-signature">Nicole</p>
+                <div className='signature-image'></div>
+                </div>
+            <QuickLinks />
+            <Footer />
+                </>
+    );
+};
+
+export default OurStory;
