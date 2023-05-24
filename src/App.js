e985af24571b722efeac9aa210b1bac0d09b583d
David Whipple <dwhipp88@gmail.com>
2023-05-23 19:54:19 -0600

    fixed all bugs, and unexpected behaviours regardings styling by setting up global css reset inside app.styles.css

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import OurStory from './components/our-story/our-story.component';
 import Foundations from './components/foundations/foundations.components';
 import TermsOfUse from './components/terms-of-use/terms-of-use.component';
 import Returns from './components/returns/returns.components';
-// import './App.styles.css';
+import './App.styles.css';
 
 const App = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
             <Route path="/pages/our-story" element={<OurStory />} />
-            <Route path="/pages/foundations" element={<Foundations />} />
-            <Route path="/pages/terms-of-use" element={<TermsOfUse />} />
+            <Route
+                path="/pages/foundations"
+                element={<Foundations />}
+            />
+            <Route
+                path="/pages/terms-of-use"
+                element={<TermsOfUse />}
+            />
             <Route path="/pages/returns" element={<Returns />} />
         </Routes>
     );
