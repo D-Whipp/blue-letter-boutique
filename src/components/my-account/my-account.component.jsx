96d91eaf4a4ba8d53c47acebd83ca42a3d5eb0c6
David Whipple <dwhipp88@gmail.com>
2023-06-15 17:09:36 -0600

    testing out some my-account component basics, I can access information after the user logins for the my-account page but a refresh breaks the page

--- src/components/my-account/my-account.component.jsx
+++ src/components/my-account/my-account.component.jsx
@@ -9,19 +9,18 @@ import QuickLinks from '../../routes/quick-links/quick-links.component';
 import Footer from '../footer/footer.component';
 
 const MyAccount = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const { currentUser } = useContext(UserContext);
 
     console.log('My Account Current User: ', currentUser);
-    console.log('My Account setCurrentUser: ', setCurrentUser);
 
-    // console.log(UserCredentialImpl);
     return (
         <>
             <Promotion />
             <Navigation />
 
             <div className="my-account-container">
-                Todo: Create My Account Page
+            Todo: Create My Account Page
+            <p>{currentUser.email}</p>
             </div>
 
             <QuickLinks />
