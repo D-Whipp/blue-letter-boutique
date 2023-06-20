a69864a883814875ceffb1ece6d4cdfe7eddef01
David Whipple <dwhipp88@gmail.com>
2023-06-19 20:17:52 -0600

    added in our addToCart feature

--- src/components/contexts/cart.context.jsx
+++ src/components/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
     // find out if cartItems contains a productToAdd
@@ -33,6 +33,12 @@ export const CartContext = createContext({
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(()=>{
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+        setCartCount(newCartCount);
+    }, [cartItems])
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
@@ -43,6 +49,7 @@ export const CartProvider = ({ children }) => {
         setIsCartOpen,
         addItemToCart,
         cartItems,
+        cartCount
     };
 
     return (
