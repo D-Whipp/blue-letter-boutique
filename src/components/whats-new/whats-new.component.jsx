93bd0f89483828d26a95277b08cb4512c72f4a01
David Whipple <dwhipp88@gmail.com>
2023-06-27 16:38:14 -0600

    completed whats-new.component and styling

--- src/components/whats-new/whats-new.component.jsx
+++ src/components/whats-new/whats-new.component.jsx
@@ -9,55 +9,26 @@ import ProductCard from '../product-card/product-card.component';
 import { useContext, useState, useEffect } from 'react';
 import { CategoriesContext } from '../../components/contexts/categories.context';
 
-// import { useParams } from 'react-router-dom';
-
 const WhatsNew = () => {
-    // const { category } = useParams();
-    // console.log('Category useParams: ', category);
-
     const { categoriesMap } = useContext(CategoriesContext);
     const [products, setProducts] = useState([]);
-    // const [newItems, setNewItems] = useState([]);
 
-    // console.log(products);
 
     useEffect(() => {
-        setProducts(categoriesMap);
+        const newItemsArray = [
+            categoriesMap.dresses[2],
+            categoriesMap.hats[5],
+            categoriesMap.hats[3],
+            categoriesMap.pants[3],
+            categoriesMap.shirts[3],
+            categoriesMap.shirts[6],
+            categoriesMap.shoes[4],
+            categoriesMap.shoes[2],
+            categoriesMap.dresses[4],
+        ];
+
+        setProducts(newItemsArray);
     }, [categoriesMap]);
-
-    // useEffect(() => {
-    //     setNewItems(products);
-    // }, [products])
-    // let newItems = [];
-
-    // if (products.length) {
-    const newItems = [
-        products.dresses[2],
-        products.hats[5],
-        products.hats[3],
-        products.pants[3],
-        products.shirts[3],
-        products.shirts[6],
-        products.shoes[4],
-        products.shoes[2],
-        products.dresses[4],
-    ];
-    // }
-    // console.log('categoriesMap: ', categoriesMap)
-    // console.log('products: ', products);
-    // console.log('setProducts: ', setProducts)
-    // let newItems;
-
-    // if (products) {
-    // }
-    // this.setState = /
-
-    // console.log('this: ', this)
-    // console.log('new item: ', newItems);
-
-    // useEffect(() => {
-    //     setProducts(categoriesMap[category]);
-    // }, [category, categoriesMap])
 
     return (
         <>
@@ -67,13 +38,13 @@ const WhatsNew = () => {
             <div className="whats-new-container">
                 <h2>what's new</h2>
                 <div className="whats-new-content-container">
-                    {newItems.length > 0 &&
-                        newItems.map((newItem) => (
-                            <ProductCard
-                                key={newItem.id}
-                                product={newItem}
-                            />
-                        ))}
+                {products &&
+                    products.map((product) => (
+                        <ProductCard
+                            key={product.id}
+                            product={product}
+                        />
+                    ))}
                 </div>
             </div>
             <QuickLinks />
@@ -82,17 +53,4 @@ const WhatsNew = () => {
     );
 };
 
-// {newItems.length
-//     ? newItems.map((newItem) => (
-//           <ProductCard
-//               key={newItem.id}
-//               product={newItem}
-//           />
-//       ))
-//     : console.log(
-//           'Failed to load new items array'
-//       )}
-
-// {newItems.length &&
-//     newItems.map((newItem) => ( <ProductCard key={newItem.id} product={newItem} />))}
 export default WhatsNew;
