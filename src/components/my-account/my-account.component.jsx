2318b013861c2d0ba549253f55126dcb79669eaa
David Whipple <dwhipp88@gmail.com>
2023-06-29 18:26:48 -0600

    exploring user management with my-account.component and google docs

--- src/components/my-account/my-account.component.jsx
+++ src/components/my-account/my-account.component.jsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import { UserContext } from '../contexts/user.context';
 
 import { Link } from 'react-router-dom';
+import { getAuth, updateProfile } from 'firebase/auth';
 
 import Promotion from '../../routes/promotion/promotion.component';
 import Navigation from '../../routes/navigation/navigation.component';
@@ -12,6 +13,18 @@ import Footer from '../footer/footer.component';
 
 const MyAccount = () => {
     const { currentUser } = useContext(UserContext);
+    const auth = getAuth();
+    // updateProfile(auth.currentUser, {
+    //     displayName: 'Miles M.',
+    //     photoUrl:
+    //         'https://ik.imagekit.io/dwimageKit/blue-letter-boutique/hats/orange-fall-hat.jpg?updatedAt=1683944831199',
+    // })
+    //     .then(() => {
+    //         alert('profile updated!');
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
 
     console.log('My Account Current User: ', currentUser);
 
@@ -22,8 +35,10 @@ const MyAccount = () => {
 
             <div className="my-account-container">
                 {currentUser ? (
-                    <div className='my-account-content'>
-                    <h2>Welcome back {currentUser.email}!</h2>
+                    <div className="my-account-content">
+                        <h2>
+                            Welcome back {currentUser.displayName}
+                        </h2>
                     </div>
                 ) : (
                     <div className="login-needed">
