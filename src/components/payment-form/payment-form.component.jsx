f94548ef4eaa4e2135872edf13fc0d67badbf4d0
David Whipple <dwhipp88@gmail.com>
2023-07-18 18:43:14 -0600

    FUNCTIONALITY UPDATE: brought in Stripe api for payments, was able to successfully test payment intent

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,67 @@
 import './payment-form.styles.css';
 
-import { CardElement } from '@stripe/react-stripe-js';
+import {useState} from 'react';
+
+import {
+    CardElement,
+    useStripe,
+    useElements,
+} from '@stripe/react-stripe-js';
 
 const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const response = await fetch(
+            '/.netlify/functions/create-payment-intent',
+            {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ amount: 10000 }),
+            }
+        ).then((response) => response.json());
+
+        const {
+            paymentIntent: { client_secret },
+        } = response;
+
+        console.log('Client Secret: ', client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(
+            client_secret,
+            {
+                payment_method: {
+                    card: elements.getElement(CardElement),
+                    billing_details: {
+                        name: 'David Whipple',
+                    },
+                },
+            }
+        );
+
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Successful');
+            }
+        }
+    };
+
     return (
         <div className="payment-form-container">
+            <h2>Credit Card Payment: </h2>
             <CardElement />
-            <button>Pay Now</button>
+            <button onClick={paymentHandler}>Pay Now</button>
         </div>
     );
 };
